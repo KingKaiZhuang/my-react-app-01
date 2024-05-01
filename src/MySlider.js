80d6514010bf7645e72888c34333a8bebb6f9c22
KingKaiZhuang <5b0g0024@stust.edu.tw>
2024-05-01 14:07:20 +0800

    add slider

---
+++ src/MySlider.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+function MySlider() {
+    const [value, setValue] = useState(128);
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    }
+    return(
+        <div>
+            <input type="range" min="0" max="255" value={value} onChange={handleChange} />
+            <span>{value}</span>
+        </div>
+    );
+}
+
+export default MySlider;
