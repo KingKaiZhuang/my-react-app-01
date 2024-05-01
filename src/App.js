996628688c3314911be3400beda991443e0d6872
KingKaiZhuang <5b0g0024@stust.edu.tw>
2024-05-01 14:34:16 +0800

    Add sass as a devDependency and update slider styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import MySlider from './MySlider';
 function App() {
   return (
     <div className="App">
-      <MySlider />
+      <header className='slider-item'>
+        <h2>5b0g0024 Zhuang Slider</h2>
+        <MySlider />
+        <MySlider />
+        <MySlider />
+      </header>
     </div>
   );
 }
--- src/MySlider.js
+++ src/MySlider.js
@@ -1,13 +1,16 @@
 import { useState } from "react";
+import './Slider.scss';
 function MySlider() {
     const [value, setValue] = useState(128);
     const handleChange = (e) => {
         setValue(e.target.value);
     }
     return(
-        <div>
-            <input type="range" min="0" max="255" value={value} onChange={handleChange} />
-            <span>{value}</span>
+        <div className="myslider">
+            <div className="slider-group">
+                <input type="range" min="0" max="255" value={value} onChange={handleChange} />
+                <span>{value}</span>
+            </div>
         </div>
     );
 }
